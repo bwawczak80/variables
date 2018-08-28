2310d70336cbba64606fc3567beb2ecf73f3202a
Brian Wawczak <bwawczak@students.mchenry.edu>
2018-08-28 12:18:03 -0500

    Initial commit

---
+++ src/main.java
@@ -0,0 +1,24 @@
+public class main {
+    public static void main(String[] args){
+
+        byte bOne = 17;
+        short sOne = 67;
+        int iNum = 54;
+        long lNum = 2157684L;
+        float fNum = 2.6f;
+        double dNum = 1.2;
+        boolean bFlag = false;
+        char cDigit = 'b';
+
+        System.out.println("byte example:  " + sOne);
+        System.out.println("short example:  " + bOne);
+        System.out.println("int example:  " + iNum);
+        System.out.println("long example:  " + lNum);
+        System.out.println("float example:  " + fNum);
+        System.out.println("double example:  " + dNum);
+        System.out.println("boolean example:  " + bFlag);
+        System.out.println("char example:  " + cDigit);
+
+
+    }
+}
